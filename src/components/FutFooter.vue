86204cda12788e7a352f1fec2db1147d2ab732aa
matostr98 <matostr98@gmail.com>
2020-10-05 18:50:05 +0200

    footer icons and agenda fixes

--- src/components/FutFooter.vue
+++ src/components/FutFooter.vue
@@ -1,21 +1,33 @@
 <template>
   <v-footer dark padless>
-    <link href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" rel="stylesheet">
+    <link
+      href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+      rel="stylesheet"
+    />
     <v-card flat tile class="primary white--text text-center" width="100%">
-<!--      <v-card-text>TODO:icons-->
-<!--        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white&#45;&#45;text" icon>-->
-<!--          <v-icon size="24px">{{ icon }}</v-icon>-->
-<!--        </v-btn>-->
-<!--      </v-card-text>-->
+      <v-card-text class="mt-n1 mb-n7">
+        <v-btn
+          v-for="(icon, index) in icons"
+          :key="index"
+          class="mx-4 white--text"
+          icon
+          :href="icon.url"
+          target="_blank"
+        >
+          <v-icon size="24px">{{ icon.fa }}</v-icon>
+        </v-btn>
+      </v-card-text>
 
       <v-card-text class="white--text pt-6">
-        Niniejszy serwis znajduje się na serwerze Politechniki Poznańskiej.  Za jego treść odpowiedzialny jest Samorząd Studentów Politechniki Poznańskiej. Kontakt do administratora podany jest w zakładce Kontakt.
+        Za treść niniejszego serwisu odpowiedzialny jest Samorząd Studentów
+        Politechniki Poznańskiej.
       </v-card-text>
 
       <v-divider></v-divider>
 
       <v-card-text class="white--text">
-        &copy;{{ new Date().getFullYear() }} — <strong>Samorząd Studentów Politechniki Poznańśkiej</strong>
+        &copy;{{ new Date().getFullYear() }} —
+        <strong>Samorząd Studentów Politechniki Poznańskiej</strong>
       </v-card-text>
     </v-card>
   </v-footer>
@@ -23,7 +35,23 @@
 
 <script>
 export default {
-  name: "FutFooter"
+  name: "FutFooter",
+  data: () => ({
+    icons: [
+      {
+        fa: "fab fa-facebook",
+        url: "https://www.facebook.com/SamorzadStudentowPolitechnikiPoznanskiej"
+      },
+      {
+        fa: "fab fa-instagram",
+        url: "https://www.instagram.com/samorzadpp/"
+      },
+      {
+        fa: "fas fa-envelope",
+        url: "mailto:samorzad@samorzad.put.poznan.pl"
+      }
+    ]
+  })
 };
 </script>
 
