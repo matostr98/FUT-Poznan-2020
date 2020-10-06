48fb672b2d05bd968d5b629cd1b5f14bbfbab914
matostr98 <matostr98@gmail.com>
2020-10-06 11:58:01 +0200

    fixed bug with not centering image on sm

--- src/components/Home/accommodation/Accommodation.vue
+++ src/components/Home/accommodation/Accommodation.vue
@@ -3,19 +3,24 @@
     <v-container fluid>
       <component-title text="Zakwaterowanie" />
       <v-row justify="center" align="center">
-        <v-col cols="12" lg="7" md="9">
-          <v-row justify="center">
-            <v-col cols="6">
+        <v-col cols="11" lg="7" md="9">
+          <v-row justify="center" align="center">
+            <!--            <v-spacer />-->
+            <v-col cols="12" xl="6" lg="6" md="6" sm="7">
               <v-img
                 src="@/assets/accommodation/andersia.jpg"
                 max-height="500"
                 max-width="500"
               />
             </v-col>
-            <v-col cols="5" class="align-content-center">
-<!--              <blockquote class="blockquote text-h5 black&#45;&#45;text">-->
-<!--                {{ quote }}-->
-<!--              </blockquote>-->
+            <v-col
+              cols="12"
+              xl="6"
+              lg="6"
+              md="6"
+              sm="10"
+              class="align-content-center"
+            >
               <p class="mt-12">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin
                 nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras
@@ -36,7 +41,7 @@
 import ComponentTitle from "@/components/general/ComponentTitle";
 export default {
   name: "Accommodation",
-  components: {ComponentTitle}
+  components: { ComponentTitle }
 };
 </script>
 
