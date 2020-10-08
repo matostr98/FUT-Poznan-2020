aef37245455c569050a0f9e0d52ab11df1085254
matostr98 <matostr98@gmail.com>
2020-10-08 17:38:23 +0200

    sspp logo fixes

--- src/components/Home/partners/OrganizerCard.vue
+++ src/components/Home/partners/OrganizerCard.vue
@@ -1,9 +1,9 @@
 <template>
   <v-card class="elevation-0 transparent">
-    <v-card-text class="text-xs-center hidden-xs-only">
+    <v-card-text class="text-center hidden-xs-only">
       <img :src="require(`@/assets/partners/${src}`)" height="200" />
     </v-card-text>
-    <v-card-text class="text-xs-center hidden-sm-and-up">
+    <v-card-text class="text-center hidden-sm-and-up">
       <img :src="require(`@/assets/partners/${src}`)" height="150" />
     </v-card-text>
   </v-card>
